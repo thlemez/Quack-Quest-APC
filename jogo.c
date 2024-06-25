c61ace63422e3da0ff899f7381130fc9eb13e941
matheuslemesam@gmail.com <matheuslemesam@gmail.com>
2024-06-25 16:34:47 -0300

    code fonte

--- jogo.c
+++ jogo.c
@@ -6,6 +6,8 @@ Texture2D pato; //declarar pato
 Texture2D cacador; //declarar caçador
 Texture2D ovo; //declarar ovo
 Texture2D pedra; //declarar pedra
+Texture2D defeat; //declarar derrota
+Texture2D victory; //declarar victory
 //------------------------------------------------------------------------------------
 int main()
 {
@@ -21,6 +23,15 @@ int main()
     const int screenHeight = 720; //altura em pixels
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window"); //abrir janela
+    InitAudioDevice();
+    
+    Sound sounddefeat = LoadSound("audio/fiasco-154915.mp3");
+
+    Sound soundwin = LoadSound("audio/Victory.mp3");
+    
+    defeat = LoadTexture("texture/defeat.png"); //linkar img derrota
+
+    victory = LoadTexture("texture/victory.png"); //linkar img victory
 
     lago = LoadTexture("texture/lago.png"); //linkar img lago
 
@@ -32,12 +43,17 @@ int main()
 
     pedra = LoadTexture("texture/pedra.png"); //linkar img pedra
 
-    Vector2 cord1 = {0,-4}; //valor inicial x e y do patinho
+    Vector2 cord1 = {0,-10}; //valor inicial x e y do patinho
 
     Vector2 cord2 = {0, 0}; //valor inicial x e y do caçador
+    
+    Vector2 posicao_pedra[100]; 
+
 
     SetTargetFPS(60);               //quadros por segundo (FPS)
     //--------------------------------------------------------------------------------------
+
+
 
     //loop jogo
     while (!WindowShouldClose())    // Detectar fechar janela ou esc
@@ -70,10 +86,12 @@ int main()
             baixo = true;
         }
 
-        if(direita == true)cord1.x += 1.2f;
-        if(esquerda == true)cord1.x -= 1.2f;
-        if(cima == true)cord1.y -= 1.2f;
-        if(baixo == true)cord1.y += 1.2f;
+        Vector2 NewPos = cord1; // Armazenar temporariamente novas posições na variavel NewPos
+        
+        if(direita) NewPos.x += 1.2f; //movimenta pra direita na velocidade 1.2 quadrados por clique
+        if(esquerda) NewPos.x -= 1.2f; //movimenta pra esquerda na velocidade 1.2 quadrados por clique
+        if(cima) NewPos.y -= 1.2f; //movimenta pra cima na velocidade 1.2 quadrados por clique
+        if(baixo) NewPos.y += 1.2f; //movimenta pra baixo na velocidade 1.2 quadrados por clique
         //----------------------------------------------------------------------------------
 
         //verificar se o pato saiu da janela e ent teletransportar ele pro outro lado
@@ -82,7 +100,51 @@ int main()
         if(cord1.x < -50) cord1.x = screenWidth;
         if(cord1.y > screenHeight + 50) cord1.y = 0;
         if(cord1.y < -50) cord1.y = screenHeight;
+
+         //sistema de colisão com pedras
+        //----------------------------------------------------------------------------------
         
+        bool collision = false; // inicializar colisão como falso
+
+        for (int i = 0; i < 5; i++){
+            
+            // Calcular caixas delimitadoras para pato e pedra
+            float patoWidth = pato.width * 0.06f;
+            float patoHeight = pato.height * 0.06f;
+            float pedraWidth = pedra.width * 0.08f;
+            float pedraheight = pedra.height * 0.08f;
+
+            // Determinar as posições do pato e pedra de todas as direções
+
+                //pato
+                float patoLeft = NewPos.x;
+                float patoRight = NewPos.x + pato.width;
+                float patoUp = NewPos.y;
+                float patoDown = NewPos.y + pato.height;
+
+                //pedra
+                float pedraLeft = posicao_pedra[i].x;
+                float pedraRight = posicao_pedra[i].x + pedra.width;
+                float pedraUp = posicao_pedra[i].y;
+                float pedraDown = posicao_pedra[i].y + pedra.height;
+
+            // Verificar se as caixas colidem e igualar "colission" como True
+            
+            if(patoRight > pedraLeft && patoLeft < pedraRight){
+                if(patoUp < pedraDown && patoDown > pedraUp){
+                    collision = true; 
+                    break;
+                }
+            }
+        }
+            //Se nao colidir o pato toma aquela posicao pra ele, ou seja, ele continua andando
+            
+            if(!collision){
+                cord1 = NewPos;
+            }
+
+
+
         //----------------------------------------------------------------------------------
         /*
         if (IsKeyDown(KEY_D)) cord1.x += 0.0f;
@@ -137,9 +199,9 @@ int main()
         caçador andar//
         */
         
-        //----------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------        
 
-        //sistema de colisão
+    
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -156,6 +218,52 @@ int main()
 
             //DrawText("Mova o pato", 0, 100, 20, LIGHTGRAY);
 
+            //pedras
+
+                // Definição do tamanho da grade
+                const int gridSizeX = 20;
+                const int gridSizeY = 20;
+
+                // Criação e inicialização da grade
+                bool grade[gridSizeX][gridSizeY];
+
+                // Inicialização da grade com obstáculos em forma de labirinto
+                for (int x = 0; x < gridSizeX; x++) {
+                    for (int y = 0; y < gridSizeY; y++) {
+                        if (x == 0 || x == gridSizeX - 1 || y == 0 || y == gridSizeY - 1) {
+                            grade[x][y] = true; // borda do labirinto
+                        } else if ((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0)) {
+                            grade[x][y] = true; // pedras em forma de labirinto
+                        } else {
+                            grade[x][y] = false; // espaço vazio
+                        }
+                    }
+                }
+
+                // Desenho da grade
+                void DrawGrid(int x, float y);
+                {
+                    for (int x = 0; x < gridSizeX; x++) {
+                        for (int y = 0; y < gridSizeY; y++) {
+                            if (grade[x][y]) {
+                                DrawTextureEx(pedra, (Vector2){x * 20, y * 20}, 0.0, 1.0, WHITE);
+                            }
+                        }
+                    }
+                }
+
+                // Atualiza o loop para checar as colisões da grade
+                while (!WindowShouldClose()) {
+
+                    
+                int x = cord1.x / 20;
+                int y = cord1.y / 20;
+                if (grade[x][y]) {
+                    // colisao detectada
+                    PlaySound(sounddefeat);
+                    DrawTexture(defeat, screenWidth / 2 - defeat.width / 2, screenHeight / 2 - defeat.height / 2, WHITE);
+                }
+
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
@@ -167,3 +275,5 @@ int main()
 
     return 0;
 }
+}
+
